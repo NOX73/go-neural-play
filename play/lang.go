8e9a81c22dfd6a39b81ad26e2a2a2e980e245212
Rozhnov Alexandr <nox73@ya.ru>
2014-02-03 01:43:24 +0400

    Lang sample

--- play/lang.go
+++ play/lang.go
@@ -8,41 +8,95 @@ import (
 	"github.com/NOX73/go-neural/persist"
 	"log"
 	"os"
+	"os/signal"
 )
 
 func LangMain() {
+	n := loadNetwork()
 
 	//createLangNetwork()
-	lernEngine()
+	testEngine(n)
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+lernLoop:
+	for !checkEngine(n) {
+
+		lernEngine(n)
+
+		select {
+		case <-c:
+			log.Println("Interrupt !")
+			break lernLoop
+		default:
+		}
+
+	}
+	testEngine(n)
+
+	saveNetwork(n)
 }
 
-func lernEngine() {
-	//e := loadEngine()
-	n := loadNetwork()
+var (
+	//gosamplePath = "/Users/nox73/kaize/docker/archive/archive_test.go"
+	//rbsamplePath = "/Users/nox73/kaize/sfk/lib/sfk/generators/app/templates/controllers/root.rb"
+	gosamplePath = "./json/sample/sample.go"
+	rbsamplePath = "./json/sample/sample.rb"
+)
+
+func checkEngine(n *neural.Network) bool {
+	gosample := getSampleFromFile(gosamplePath)
+	rbsample := getSampleFromFile(rbsamplePath)
+
+	var out []float64
+
+	out = n.Calculate(gosample)
+	if out[0] < 0.8 || out[1] > 0.2 {
+		log.Println("CheckFailed", gosamplePath, out)
+		return false
+	}
+
+	out = n.Calculate(rbsample)
+	if out[1] < 0.8 || out[0] > 0.2 {
+		log.Println("CheckFailed", rbsamplePath, out)
+		return false
+	}
+
+	return true
+}
+
+func testEngine(n *neural.Network) {
+	gosample := getSampleFromFile(gosamplePath)
+	rbsample := getSampleFromFile(rbsamplePath)
+
+	log.Println(gosamplePath, n.Calculate(gosample))
+	log.Println(rbsamplePath, n.Calculate(rbsample))
+}
+
+func lernEngine(n *neural.Network) {
 
 	gofiles := getGoFiles()
 	rbfiles := getRbFiles()
 
-	min := 5
-	//min := len(gofiles)
+	//min := 5
+	min := len(gofiles)
 
 	if min > len(rbfiles) {
 		min = len(rbfiles)
 	}
 
 	for i := 0; i < min; i++ {
-		log.Println("Iteration #", i)
+		//log.Println("Iteration #", i)
 
 		lernLangFile(n, gofiles[i], []float64{1, 0})
-		lernLangFile(n, rbfiles[i], []float64{0, 1})
+		//lernLangFile(n, rbfiles[i], []float64{0, 1})
 	}
 
-	saveNetwork(n)
 }
 
 func lernLangFile(n *neural.Network, path string, out []float64) {
-	log.Println("Lerning ", path)
+	//log.Println("Lerning ", path)
 	sample := getSampleFromFile(path)
 	lern.Lern(n, sample, out, 0.1)
 }
